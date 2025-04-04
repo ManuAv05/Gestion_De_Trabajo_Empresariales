1a625d990f97c6fff03f5b584dc4db53a7d52cc7
Manuel Avila Dugo <manuelaviladugo@gmail.com>
2025-04-04 11:55:28 +0200

    Proyecto

---
+++ Gestion_Proyectos/Proyecto.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public final class Proyecto {
+    private String nombre;
+
+    private String descripcion;
+
+    private String[] lista_personas;
+
+    private String estado_proyecto;
+
+    private String fecha_inicio;
+
+    private String fecha_fin;
+
+    public void Proyecto(final String nombre, final String descripcion, final List<String> lista_personas, final String estado_proyecto, final String fecha_inicio, final String fecha_fin) {
+    }
+
+}
